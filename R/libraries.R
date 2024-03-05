476a2ec6b89315482eda4c3b450aeb936510f644
deboonstra <daniel-boonstra@uiowa.edu>
2024-03-05 15:26:36 -0600

    Updating list of required libraries

---
+++ R/libraries.R
@@ -0,0 +1,6 @@
+library(bit, warn.conflicts = FALSE, quietly = TRUE, verbose = FALSE)
+library(bit64, warn.conflicts = FALSE, quietly = TRUE, verbose = FALSE)
+library(dplyr, warn.conflicts = FALSE, quietly = TRUE, verbose = FALSE)
+library(stats, warn.conflicts = FALSE, quietly = TRUE, verbose = FALSE)
+library(astsa, warn.conflicts = FALSE, quietly = TRUE, verbose = FALSE)
+library(parallel, warn.conflicts = FALSE, quietly = TRUE, verbose = FALSE)
